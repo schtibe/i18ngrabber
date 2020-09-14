950ea9fa222f9653151e5faf2ca3cc8c28a8e9ba
Stefan Heinemann <stefan.heinemann@codedump.ch>
2020-09-14 09:08:28 +0200

    Introduce linting

--- src/extension.ts
+++ src/extension.ts
@@ -1,101 +1,106 @@
 import { dirname } from "path";
 import { mkdirSync } from "fs";
-import * as jsonFile from 'jsonfile';
+import * as jsonFile from "jsonfile";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-const set = require('lodash.set');
+import set = require("lodash.set");
 
-const addToLocaleFile = (placeholder: string, text: string, translationFile: string): Promise<string> => {
-	return new Promise((resolve, reject) => {
+const addToLocaleFile = (
+  placeholder: string,
+  text: string,
+  translationFile: string,
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    jsonFile.readFile(
+      translationFile,
+      (e: unknown, obj: Record<string, unknown>) => {
+        /* eslint-disable no-irregular-whitespace */
+        obj = obj || {};
 
-		jsonFile.readFile(translationFile, (e: any, obj: any) => {
-			/* eslint-disable no-irregular-whitespace */
-			obj = obj || {};
+        // this will transform dot notation
+        set(obj, placeholder, text);
 
-			// this will transform dot notation
-			set(obj, placeholder, text);
-
-			jsonFile.writeFile(translationFile, obj, { spaces: 4 }, (err: any) => { reject(err); });
-			resolve();
-		});
-
-		return translationFile;
-	});
+        jsonFile.writeFile(
+          translationFile,
+          obj,
+          { spaces: 4 },
+          (err: unknown) => {
+            reject(err);
+          },
+        );
+        resolve();
+      },
+    );
+  });
 };
 
 const createIfNotExist = (translationFile: string) => {
-	const directory = dirname(translationFile);
-	return mkdirSync(directory);
+  const directory = dirname(translationFile);
+  return mkdirSync(directory);
 };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
+  const disposable = vscode.commands.registerCommand(
+    "i18ngrabber.grabtext",
+    () => {
+      const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "i18ngrabber" is now active!');
+      if (!editor) {
+        return; // No open text editor
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('i18ngrabber.grabtext', () => {
-		// The code you place here will be executed every time your command is executed
+      const selection = editor.selection;
+      const text = editor.document.getText(selection);
 
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return; // No open text editor
-		}
+      if (text === "") {
+        vscode.window.showInformationMessage("Please select your text");
+        return;
+      }
 
-		var selection = editor.selection;
+      vscode.window
+        .showInputBox({
+          value: "Enter The name of the placeholder",
+        })
+        .then((placeholder) => {
+          if (!placeholder) {
+            // TODO error handling
+            return;
+          }
 
-		var text = editor.document.getText(selection);
+          // Display a message box to the user
+          vscode.window.showInformationMessage(
+            "Selected text:" + text + " input " + placeholder,
+          );
 
-		if (text === '') {
-			vscode.window.showInformationMessage('Please select your text');
-			return;
-		}
+          editor?.edit((editBuilder) => {
+            const newString = `{{ $t('${placeholder}') }}`;
+            editBuilder.replace(selection, newString);
+          });
 
-		vscode.window.showInputBox({
-			value: "Enter The name of the placeholder"
-		}).then(placeholder => {
-			if (!placeholder) {
-				// TODO error handling
-				return;
-			}
+          const config = vscode.workspace.getConfiguration("i18nGrabber");
+          const translationFile = config
+            .get("translationFileLocation", "locale/en.json")
+            .toString();
 
-			// Display a message box to the user
-			vscode.window.showInformationMessage(
-				'Selected text:' + text + " input " + placeholder);
+          createIfNotExist(translationFile);
 
-			editor?.edit((editBuilder) => {
-				const newString = `{{ $t('${placeholder}') }}`;
-				editBuilder.replace(
-					selection,
-					newString
-				);
-			});
+          addToLocaleFile(placeholder, text, translationFile)
+            .then(() => {
+              vscode.window.showInformationMessage(
+                `Added placeholder ${placeholder} to ${translationFile}`,
+              );
+            })
+            .catch((error) => {
+              vscode.window.showErrorMessage(error);
+            });
+        });
+    },
+  );
 
-			const config = vscode.workspace.getConfiguration('i18nGrabber');
-			const translationFile = config.get('translationFileLocation', 'locale/en.json').toString();
-
-			createIfNotExist(translationFile);
-
-			addToLocaleFile(placeholder, text, translationFile).then((file: string) => {
-
-				vscode.window.showInformationMessage(
-					`Added placeholder ${placeholder} to ${translationFile}`
-				);
-			}).catch((error) => {
-				vscode.window.showErrorMessage(error);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+// export function deactivate() {}
